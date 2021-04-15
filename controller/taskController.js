701c0428f1c437d339316257bfd844caec2b3611
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-15 14:43:28 +0300

    Add task controller

---
+++ controller/taskController.js
@@ -0,0 +1,72 @@
+const { User, Task } = require('../models');
+const createError = require('http-errors');
+
+
+module.exports.createTask = async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    const task = await Task.create(body);
+
+    if (!task) {
+      return next(createError(400, 'tasks not created'));
+    }
+
+    res.status(201).send({ data: task });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getTasks = async (req, res, next) => {
+  try {
+    const {pagination} = req;
+    const tasks = await Task.findAll({...pagination});
+
+    if (!tasks.length) {
+      return next(createError(404, 'tasks not found'));
+    }
+
+    res.status(200).send({ data: tasks });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateTask = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { id },
+    } = req;
+
+    const [rows, [task]] = await Task.update(body, {
+      where: { id },
+      returning: true,
+    });
+
+    if (rows === 0) {
+      return next(createError(404, 'task not found'));
+    }
+
+    res.status(200).send({ data: task });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteTask = async (req, res, next) => {
+  try {
+    const { params: {id} } = req;
+
+    const rows = await Task.destroy({ where: { id } });
+
+    if (rows === 0) {
+      return next(createError(404, 'task not found'));
+    }
+
+    res.status(200).send({ data: id });
+  } catch (err) {
+    next(err);
+  }
+};
