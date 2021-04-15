8e92cfe97d58d03a09d1b011cd11ae5247c25111
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-04-15 14:42:23 +0300

    Add task migration

---
+++ migrations/210210414164553-create-task.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      deadline: {
+        type: Sequelize.DATEONLY,
+      },
+      isDone: {
+        field: 'is_done',
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tasks');
+  },
+};
